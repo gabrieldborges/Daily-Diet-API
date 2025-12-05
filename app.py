1440449fb5cfb4d9ba4bdccb798fe13060cf54a0
gabrieldborges <gabrieldantas1404@icloud.com>
2025-12-05 19:05:31 -0300

    Conected app to MySQL database

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask
-
+from database import db
 from models.meals import Meals
-
-print(Meals(id=1,description="Arroz com feijão", is_in_diet=True).to_dict())
-
-
-
 
 
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/Daily-Diet-API-db'
+db.init_app(app)
 
-
-
+# meals = Meals(id=1,description="Arroz com feijão",is_in_diet=True)
 
 
 
--- models/meals.py
+++ models/meals.py
@@ -4,4 +4,14 @@ from datetime import datetime, timezone
 class Meals(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(140), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), default=lambda : datetime.now(timezone.utc),nullable=
+    created_at = db.Column(db.DateTime(timezone=True), default=lambda : datetime.now(timezone.utc),nullable=False)
+    is_in_diet = db.Column(db.Boolean,nullable=False)
+    
+    
+    def to_dict(self):
+        return{
+            'ID' : self.id, 
+            "Description":self.description,
+            "Created at" : self.created_at,
+            "Included in diet": self.is_in_diet
+        }
