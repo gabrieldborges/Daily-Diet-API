7db5f44f505733b3584097b20a7fe01d36fb1854
gabrieldborges <gabrieldantas1404@icloud.com>
2025-12-05 21:04:42 -0300

    Fixed some bugs in PUT route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
 from models.meals import Meals
 
@@ -12,6 +12,45 @@ db.init_app(app)
 
 # meals = Meals(id=1,description="Arroz com feijão",is_in_diet=True)
 
+@app.route('/meals', methods=['POST'])
+def add_meal():
+    data = request.json
+    description = data.get("description")
+    is_in_diet = data.get("is_in_diet")
+    type_of_meal = data.get("type_of_meal")
+    
+    if description is None or is_in_diet is None or type_of_meal is None:
+        return jsonify({"message": "Missing meal information."}), 400
+
+    
+    meal = Meals(type_of_meal=type_of_meal,description=description,is_in_diet=is_in_diet)
+    db.session.add(meal)
+    db.session.commit()
+    return jsonify({"message":"Meal created successfully."})
+    
+    
+
+@app.route('/meals/<int:meal_id>', methods=['PUT'])
+def update_meal(meal_id):
+    data = request.json
+    description = data.get("description")
+    is_in_diet = data.get("is_in_diet")
+    type_of_meal = data.get("type_of_meal")
+
+    meal = Meals.query.get(meal_id)
+    if not meal:
+        return jsonify({"message": "Meal not found."}), 404
+    
+    if meal and (description or is_in_diet or type_of_meal):
+        meal.description = description if description is not None else meal.description
+        meal.is_in_diet = is_in_diet if is_in_diet is not None else meal.is_in_diet
+        meal.type_of_meal = type_of_meal if type_of_meal is not None else meal.type_of_meal
+        db.session.commit() 
+        return jsonify({"message":"Meal updated successfully."})
+    
+    return jsonify({"message":"Not able to update on current given information."}),400
+    
+    
 
 
 
--- models/meals.py
+++ models/meals.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone
 
 class Meals(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    type_of_meal = db.Column(db.String(20),nullable=False)
     description = db.Column(db.String(140), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), default=lambda : datetime.now(timezone.utc),nullable=False)
     is_in_diet = db.Column(db.Boolean,nullable=False)
@@ -11,6 +12,7 @@ class Meals(db.Model):
     def to_dict(self):
         return{
             'ID' : self.id, 
+            "Type of meal" : self.type_of_meal,
             "Description":self.description,
             "Created at" : self.created_at,
             "Included in diet": self.is_in_diet
